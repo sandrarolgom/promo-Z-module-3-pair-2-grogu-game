03f6821ee453a63320ff0632945846e9af143702
Shannnon Evora Lopes Rodrigues <shannonelr9208@icloud.com>
2024-09-05 11:29:06 +0200

    estados

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,28 @@
 // Fichero src/components/App.jsx 
 import '../scss/index.scss';
 import '../scss/body.scss';
+import { useState } from 'react';
+
+/* Primer paso:
+--> inicializar el estado de Grogu
+--> incluir la posicón inical de Grogu, de la mercancía 
+--> resultado de Grogu
+*/
+
+
+
+
 
 function App() {
+  // aquí van las variables de estado
+ const [box, setBox] = useState (0);
+ const [cookie, setCookie] = useState(['🍪','🍪','🍪']);//  dentro del array es la posición inicial
+ const [egg, setEgg] = useState(['🥚','🥚','🥚']);
+ const [frog, setFrog] = useState(['🐸','🐸','🐸']);
+ const [status, setStatus] = useState('En curso');
+ 
+
+ 
   return (
     <div className="page"> 
     <header>
@@ -10,31 +30,36 @@ function App() {
     </header>
     <main className="page">
       <section className="board">
-        <div className="cell"><div className="grogu">👣</div></div>
-        <div className="cell"></div>git 
-        <div className="cell"></div>
-        <div className="cell"></div>
-        <div className="cell"></div>
-        <div className="cell"></div>
-        <div className="cell"></div>
+        {/* aqui variables de estado SetBox */}
+        <div className="cell" ><div className="grogu">👣</div></div>
+        <div className="cell" ></div> 
+        <div className="cell" ></div>
+        <div className="cell" ></div>
+        <div className="cell" ></div>
+        <div className="cell" ></div>
+        <div className="cell" ></div>
       </section>
 
       <section>
+        {/*aqui va el evento onChange para que cambie la posicion de Grogu*/}
         <button className="dice">Lanzar Dado</button>
-        <div className="game-status">En curso</div>
+        <div className="game-status">{status}</div>
       </section>
 
       <section className="goods-container">
+        {/*variable de estado cookie*/}
         <div className="goods-item">🍪</div>
         <div className="goods-item">🍪</div>
         <div className="goods-item">🍪</div>
       </section>
       <section className="goods-container">
+        {/* variable de estado egg*/}
         <div className="goods-item">🥚</div>
         <div className="goods-item">🥚</div>
         <div className="goods-item">🥚</div>
       </section>
       <section className="goods-container">
+        {/* variable de estado frog*/}
         <div className="goods-item">🐸</div>
         <div className="goods-item">🐸</div>
         <div className="goods-item">🐸</div>
