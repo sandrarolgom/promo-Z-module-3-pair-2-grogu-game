142ea702781cb5c46ba31cf644e4882fa50a3d5c
Shannnon Evora Lopes Rodrigues <shannonelr9208@icloud.com>
2024-09-10 11:28:35 +0200

    changes position Grogu

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -4,13 +4,13 @@ function Board({container}) {
   // creamos una array de 7 posiciones con valor null
   /* Array no da la longitud y fill los datos */
   const cell = Array(7).fill(null)
-  
+
   
 
   
     return(
         <section className="board">
-          {cell.map((eachItem,i)=><div key={i} className="cell" ></div> )}
+          {cell.map((eachItem,i)=><div key={i} className="cell"> {container === i && <Grogu /> } </div> )}
         
         {/* aqui variables de estado SetBox */}
         {/*<div className="cell" > <Grogu /> </div>
